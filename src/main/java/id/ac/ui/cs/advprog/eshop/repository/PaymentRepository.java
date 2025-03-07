29ee94b377722dea851ff4e6c139b1ac132beb4d
unknown <flori.andrea.60@gmail.com>
2025-03-07 10:53:21 +0700

    [RED] Added skeleton for Payment Repository.

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -0,0 +1,21 @@
+package id.ac.ui.cs.advprog.eshop.repository;
+
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+
+import java.util.*;
+
+public class PaymentRepository {
+    private final Map<String, Payment> payments = new HashMap<>();
+
+    public Payment findById(String id) {
+        return null;
+    }
+
+    public List<Payment> findAll() {
+        return null;
+    }
+
+    public void save(Payment payment) {
+        ;
+    }
+}
